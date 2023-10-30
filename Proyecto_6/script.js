bf7693d6938ac19662f9a718ea197ea1aea075df
MiguelACoronado <125321558+MiguelACoronado@users.noreply.github.com>
2023-10-30 12:51:53 -0500

    Proyecto_Actualizado

--- Proyecto_6/script.js
+++ Proyecto_6/script.js
@@ -1,42 +1,37 @@
-const marcoPuntaje = document.querySelector('.puntaje')
-const puntajeActual= 0;
+const marcoPuntaje = document.querySelector('.puntaje');
+let puntajeActual = 0;
 const puntajeLimite = 5;
 
-const htmlMapa = Array.from(Array(puntajeLimite)).map((item,i) =>{
-    return `<div class="item item-${i}" data-pos="${i}></div>`;
+const htmlMapa = Array.from(Array(puntajeLimite)).map((item, i) => {
+    return `<div class="item item-${i}" data-pos="${i}"></div>`;
 });
+
 marcoPuntaje.innerHTML = htmlMapa.join('');
 
-document.querySelectorAll(".item").forEach(item =>{
-    item.addEventListener("mouseover", e=>{
-        console.log("Funciona");
+document.querySelectorAll(".item").forEach((item) =>{
+    item.addEventListener('mouseover',(e) =>{
+
         const posicion = item.getAttribute("data-pos");
 
-        if(puntajeActual === parseInt(posicion) + 1) {
+        if(puntajeActual === parseInt(posicion) + 1){
             return;
         }
 
-
         document.querySelectorAll(".item").forEach(cuadraditoGris => {
-            if(cuadraditoGris.classList.contains("select")){
-                cuadraditoGris.classList.remove("select")
+            if (cuadraditoGris.classList.contains("selec")) {
+                cuadraditoGris.classList.remove("selec");
             }
         })
 
-
-
-
-
-        for(let i = 0; i <= posicion; i++){
+        for(let i=0; i <= posicion; i++){
             const cuadradito = document.querySelector(`.item-${i}`);
-            if(!cuadradito.classList.contains("select")) {
-                cuadradito.classList.add("select");
+            if(!cuadradito.classList.contains("selec")) {
+                cuadradito.classList.add("selec");
             }
-        }
+        } 
         puntajeActual = parseInt(posicion) + 1;
     });
-
-    item.addEventListener("click", (e) => {
+    item.addEventListener("click",(e) => {
         const posicion = item.getAttribute("data-pos");
         puntajeActual = parseInt(posicion) + 1;
         console.log(puntajeActual);
